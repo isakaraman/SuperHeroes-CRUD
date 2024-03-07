c75fc0120134eedc0edcb3388b4654e7467b9664
İsa Karaman <77787550+isakaraman@users.noreply.github.com>
2024-03-07 15:58:43 +0300

    first commit

---
+++ SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+using SuperHeroAPI.Entities;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SuperHeroController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllSuperHeroes()
+        {
+            var result = await _context.SuperHeroes.ToListAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSuperHeroesById(int id)
+        {
+            var result = await _context.SuperHeroes.FirstOrDefaultAsync(s=>s.Id==id);
+
+            if (result == null)
+                return NotFound("Hero Not Found");
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateHero([FromBody]SuperHero id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _context.SuperHeroes.Add(id);
+            await _context.SaveChangesAsync();
+
+            return Ok("Hero added");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateHero([FromBody] SuperHero id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var result=await _context.SuperHeroes.FindAsync(id.Id);
+
+            if(result == null)
+                return NotFound("Hero not found");
+
+            result.Name = id.Name;
+            result.FirstName = id.FirstName;
+            result.LastName = id.LastName;
+            result.Place = id.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHero(int id)
+        {
+            var result = await _context.SuperHeroes.FirstOrDefaultAsync(s => s.Id == id);
+
+            if (result == null)
+                return NotFound("Hero Not Found");
+
+            _context.SuperHeroes.Remove(result);
+            await _context.SaveChangesAsync();
+
+            return Ok("Hero "+result.Name+" Deleted");
+        }
+    }
+}
---
+++ SuperHeroAPI/Entities/SuperHero.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperHeroAPI.Entities
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        [Required]
+        public string FirstName { get; set; }=string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public string Place { get; set; } = string.Empty;
+    }
+}
